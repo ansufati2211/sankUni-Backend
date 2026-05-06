8a69cf4872b35a9f2b6f1ae6062ea748105f648f
Jesus9Rojas <u23227101@utp.edu.pe>
2026-05-06 15:36:48 -0500

    Modulo de docente y alumno funcional

--- src/main/java/com/snkuni/sankuni/SankuniApplication.java
+++ src/main/java/com/snkuni/sankuni/SankuniApplication.java
@@ -2,8 +2,10 @@ package com.snkuni.sankuni;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SankuniApplication {
 
 	public static void main(String[] args) {
